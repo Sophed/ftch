9bf80ddf3f518109c5389409a9c2a0a7404c6534
Sophed <65602967+Sophed@users.noreply.github.com>
2025-09-22 21:48:35 +0000

    fix os-release distro parsing

--- src/data/distro.rs
+++ src/data/distro.rs
@@ -4,6 +4,8 @@ use std::process::Command;
 pub fn distro() -> String {
     if cfg!(target_os = "macos") {
         mac()
+    } else if cfg!(target_os = "windows") {
+        windows()
     } else {
         linux() // default
     }
@@ -11,9 +13,24 @@ pub fn distro() -> String {
 
 fn linux() -> String {
     let contents = fs::read_to_string("/etc/os-release").expect("failed to read distro name");
-    let line: Vec<&str> = contents.split("\n").collect();
-    let name: Vec<&str> = line.get(0).unwrap().split("=").collect();
-    name.get(1).unwrap().replace("\"", "")
+    let lines: Vec<&str> = contents.split("\n").collect();
+    for line in &lines {
+        if line.starts_with("PRETTY_NAME") { // priority
+            let name: Vec<&str> = line.split("=").collect();
+            return name.get(1).unwrap().replace("\"", "");
+        }
+    }
+    for line in lines {
+        if line.starts_with("NAME") { // fallback
+            let name: Vec<&str> = line.split("=").collect();
+            return name.get(1).unwrap().replace("\"", "");
+        }
+    }
+    "unknown".to_string() // terrible distro config
+}
+
+fn windows() -> String {
+    "Windows".to_string() // TODO: differentiate between w10 & w11 for proper support
 }
 
 fn mac() -> String {
@@ -21,7 +38,7 @@ fn mac() -> String {
         .arg("-productName")
         .output()
         .expect("failed to fetch product name");
-    
+
     let name = String::from_utf8_lossy(&output.stdout);
     name.trim().to_string()
 }
